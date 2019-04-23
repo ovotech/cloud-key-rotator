f6ba4ac63075447a16ae9798cbd32564d6309895
eversC <chris.every@ovoenergy.com>
2019-04-23 16:25:27 +0100

    WIP Add AWS Secret Manager functionality

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/ovotech/cloud-key-rotator/cmd"
@@ -9,16 +10,22 @@ import (
 	"github.com/ovotech/cloud-key-rotator/pkg/rotate"
 )
 
+// MyEvent type
 type MyEvent struct {
 	Name string `json:"name"`
 }
 
+//HandleRequest allows cloud-key-rotator to be used in the Lambda program model
 func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	//TODO: get config from
-	// cmd.Execute()
 	var c config.Config
+	var err error
+	if c, err = config.GetConfigFromAWSSecretManager(
+		getEnv("CKR_SECRET_CONFIG_NAME", "ckr-config"),
+		getEnv("CKR_CONFIG_TYPE", "json")); err != nil {
+		return "fail", err
+	}
 	rotate.Rotate("", "", "", c)
-	return "", nil
+	return "success", err
 }
 
 func main() {
@@ -29,6 +36,16 @@ func main() {
 	}
 }
 
+//isLambda returns true if the AWS_LAMBDA_FUNCTION_NAME env var is set
 func isLambda() (isLambda bool) {
-	return
+	return len(os.Getenv("AWS_LAMBDA_FUNCTION_NAME")) > 0
+}
+
+//getEnv returns the value of the env var matching the key, if it exists, and
+// the value of fallback otherwise
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
 }
---
+++ pkg/config/config.go
@@ -0,0 +1,112 @@
+package config
+
+import (
+	"bytes"
+	"errors"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
+	"github.com/ovotech/cloud-key-rotator/pkg/cred"
+	"github.com/ovotech/cloud-key-rotator/pkg/location"
+	"github.com/spf13/viper"
+)
+
+//Config type
+type Config struct {
+	IncludeAwsUserKeys              bool
+	Datadog                         Datadog
+	DatadogAPIKey                   string
+	RotationMode                    bool
+	CloudProviders                  []cloudProvider
+	AccountFilter                   filter
+	AccountKeyLocations             []KeyLocations
+	Credentials                     cred.Credentials
+	DefaultRotationAgeThresholdMins int
+}
+
+//cloudProvider type
+type cloudProvider struct {
+	Name    string
+	Project string
+	Self    string
+}
+
+//Datadog type
+type Datadog struct {
+	MetricEnv  string
+	MetricTeam string
+	MetricName string
+}
+
+type filter struct {
+	Mode     string
+	Accounts []ProviderServiceAccounts
+}
+
+//KeyLocations type
+type KeyLocations struct {
+	RotationAgeThresholdMins int
+	ServiceAccountName       string
+	CircleCI                 []location.CircleCI
+	GitHub                   location.GitHub
+	K8s                      []location.K8s
+}
+
+//ProviderServiceAccounts type
+type ProviderServiceAccounts struct {
+	Provider         cloudProvider
+	ProviderAccounts []string
+}
+
+const envVarPrefix = "ckr"
+
+//GetConfig returns the application config
+func GetConfig(configPath string) (c Config, err error) {
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix(envVarPrefix)
+	viper.AddConfigPath(configPath)
+	viper.SetEnvPrefix("ckr")
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	viper.ReadInConfig()
+	if err = viper.Unmarshal(&c); err != nil {
+		return
+	}
+	if !viper.IsSet("cloudProviders") {
+		err = errors.New("cloudProviders is not set")
+		return
+	}
+	return
+}
+
+// GetConfigFromAWSSecretManager grabs the cloud-key-rotator's config from
+// AWS Secret Manager
+func GetConfigFromAWSSecretManager(secretName, configType string) (c Config, err error) {
+	var secret string
+	if secret, err = getSecret(secretName); err != nil {
+		return
+	}
+	if len(secret) == 0 {
+		return c, errors.New("Unable to obtain secret value. Check user permissions and secret name")
+	}
+	viper.SetConfigType(configType)
+	viper.ReadConfig(bytes.NewBufferString(secret))
+	err = viper.Unmarshal(&c)
+	return
+}
+
+func getSecret(secretName string) (secretString string, err error) {
+	//Create a Secrets Manager client
+	svc := secretsmanager.New(session.New())
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId:     aws.String(secretName),
+		VersionStage: aws.String("AWSCURRENT"), // VersionStage defaults to AWSCURRENT if unspecified
+	}
+	var result *secretsmanager.GetSecretValueOutput
+	if result, err = svc.GetSecretValue(input); err != nil {
+		return
+	}
+	secretString = *result.SecretString
+	return
+}
