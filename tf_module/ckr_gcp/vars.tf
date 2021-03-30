1292bad56f1bbf612df5122736a83a709c846e49
Eleanor Nicholson <eleanor.nicholson@kaluza.com>
2021-03-30 17:11:16 +0100

    Improve GCP module validation and testing

--- tf_module/ckr_gcp/vars.tf
+++ tf_module/ckr_gcp/vars.tf
@@ -1,20 +1,33 @@
+variable "ckr_version" {
+  type        = string
+  description = "The Cloud Key Rotator binary version to use."
+}
+
+variable "ckr_config" {
+  type        = string
+  description = "The JSON configuration for the Cloud Key Rotator to use"
+}
+
 variable "ckr_resource_suffix" {
-  default = ""
+  default     = ""
+  type        = string
+  description = "Suffix to append to resources to avoid naming conflicts if multiple cloud key rotator modules are in use in your project"
 }
 
 variable "ckr_schedule" {
-  default = "0 10 * * 1-5"
+  default     = "0 10 * * 1-5"
+  type        = string
+  description = "Cron style schedule at which to trigger the Cloud Key rotator"
 }
 
 variable "ckr_schedule_time_zone" {
-  default = "Europe/London"
+  default     = "Europe/London"
+  type        = string
+  description = "The time zone for the scheduler job to schedule in"
 }
 
 variable "deploying_accounts" {
-  default = []
-  type    = list(string)
+  default     = []
+  type        = list(string)
+  description = "List of accounts which will be deploying the CKR terraform. This needs to be given if you are not giving the deploying accounts the iam.serviceAccountUser permission for the whole project"
 }
-
-variable "ckr_version" {}
-
-variable "ckr_config" {}
