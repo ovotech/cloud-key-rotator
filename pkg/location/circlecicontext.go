4e6549ba30973c169120048511ddc14aab62550e
eversC <chris.every@ovoenergy.com>
2021-09-09 15:09:38 +0100

    Switch CircleCI contexts to REST

--- pkg/location/circlecicontext.go
+++ pkg/location/circlecicontext.go
@@ -2,9 +2,12 @@ package location
 
 import (
 	"encoding/base64"
+	"net/http"
+
 	"github.com/ovotech/cloud-key-rotator/pkg/cred"
 
 	"github.com/CircleCI-Public/circleci-cli/api"
+	"github.com/CircleCI-Public/circleci-cli/settings"
 )
 
 // CircleCIContext type
@@ -17,13 +20,16 @@ type CircleCIContext struct {
 
 func (circleContext CircleCIContext) Write(serviceAccountName string, keyWrapper KeyWrapper, creds cred.Credentials) (updated UpdatedLocation, err error) {
 	logger.Info("Starting CircleCI context env var updates")
-
-	gqlclient := api.NewContextGraphqlClient(
-		"https://circleci.com",
-		"graphql",
-		creds.CircleCIAPIToken,
-		false,
-	)
+	cfg := settings.Config{
+		Host:         "https://circleci.com",
+		HTTPClient:   http.DefaultClient,
+		RestEndpoint: "api/v2",
+		Token:        creds.CircleCIAPIToken,
+	}
+	var restClient *api.ContextRestClient
+	if restClient, err = api.NewContextRestClient(cfg); err != nil {
+		return
+	}
 
 	provider := keyWrapper.KeyProvider
 	contextID := circleContext.ContextID
@@ -51,12 +57,12 @@ func (circleContext CircleCIContext) Write(serviceAccountName string, keyWrapper
 	}
 
 	if len(keyIDEnvVar) > 0 {
-		if err = updateCircleCIContext(contextID, keyIDEnvVar, keyWrapper.KeyID, gqlclient); err != nil {
+		if err = updateCircleCIContext(contextID, keyIDEnvVar, keyWrapper.KeyID, restClient); err != nil {
 			return
 		}
 	}
 
-	if err = updateCircleCIContext(contextID, keyEnvVar, key, gqlclient); err != nil {
+	if err = updateCircleCIContext(contextID, keyEnvVar, key, restClient); err != nil {
 		return
 	}
 
@@ -70,10 +76,10 @@ func (circleContext CircleCIContext) Write(serviceAccountName string, keyWrapper
 }
 
 func updateCircleCIContext(contextID, envVarName, envVarValue string,
-	gqlclient *api.GraphQLContextClient) (err error) {
+	restClient *api.ContextRestClient) (err error) {
 
-	if err = gqlclient.DeleteEnvironmentVariable(contextID, envVarName); err != nil {
+	if err = restClient.DeleteEnvironmentVariable(contextID, envVarName); err != nil {
 		return
 	}
-	return gqlclient.CreateEnvironmentVariable(contextID, envVarName, envVarValue)
+	return restClient.CreateEnvironmentVariable(contextID, envVarName, envVarValue)
 }
