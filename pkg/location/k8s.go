4169e93f3b6e99e3c567ac5f8393afea47b3fc8a
eversC <chris.every@kaluza.com>
2023-11-21 22:04:06 +0000

    Add updateOptions{}

--- pkg/location/k8s.go
+++ pkg/location/k8s.go
@@ -150,7 +150,7 @@ func updateK8sSecret(ctx context.Context, secretName, dataName, namespace, key s
 		return
 	}
 	secret.Data = map[string][]byte{dataName: decodedKey}
-	return k8sclient.CoreV1().Secrets(namespace).Update(ctx, secret)
+	return k8sclient.CoreV1().Secrets(namespace).Update(ctx, secret, metav1.UpdateOptions{})
 }
 
 // gkeCluster creates a GKE cluster struct
